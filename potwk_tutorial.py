24a1118ac350f2283d9622169e8256b2b5f92194
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-07 00:38:26 +0100

    created router page

--- potwk_tutorial.py
+++ potwk_tutorial.py
@@ -6,16 +6,14 @@ Created on Tue Aug  6 11:51:36 2024
 """
 import streamlit as st
 
+st.title("Paladins of the West Kingdom: How to Play")
+
+why = st.Page("why_play.py", title="Why play Paladins of the West Kingdom?", icon=":material/thumb_up:")
 worker_placement = st.Page("worker_placement.py", title="What is a worker placement game?", icon = ":material/thumb_up:")
 aim = st.Page("aim.py", title="What is the aim of the game?", icon=":material/thumb_up:")
 
-pg = st.navigation([worker_placement, aim])
+pg = st.navigation([why, worker_placement, aim])
 st.set_page_config(page_title="POTWK Tutorial", page_icon=":material/thumb_up:")
 pg.run()
 
-st.title("Paladins of the West Kingdom: How to Play")
-st.header("Why should you play Paladins of the West Kingdom?")
 
-st.subheader("Now, you may want to check out:")
-st.write("What is a worker placement game?")
-st.write("What is the aim of the game?")
---
+++ why_play.py
@@ -0,0 +1,12 @@
+"""
+Created on Wed Aug  7 00:32:30 2024
+
+@author: jd_se
+"""
+import streamlit as st
+
+st.title("Why should you play Paladins of the West Kingdom?")
+
+st.subheader("Now, you may want to check out:")
+st.write("What is a worker placement game?")
+st.write("What is the aim of the game?")
