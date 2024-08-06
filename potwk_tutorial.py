eae11958d7a6c4ac6cec9156ec23d2e920b514b2
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-07 00:41:30 +0100

    test

--- potwk_tutorial.py
+++ potwk_tutorial.py
@@ -8,14 +8,14 @@ import streamlit as st
 
 st.set_page_config(page_title="POTWK Tutorial", page_icon=":material/thumb_up:")
 
-st.title("Paladins of the West Kingdom: How to Play")
+#st.title("Paladins of the West Kingdom: How to Play")
 
-why = st.Page("why_play.py", title="Why play Paladins of the West Kingdom?", icon=":material/thumb_up:")
-worker_placement = st.Page("worker_placement.py", title="What is a worker placement game?", icon = ":material/thumb_up:")
-aim = st.Page("aim.py", title="What is the aim of the game?", icon=":material/thumb_up:")
+#why = st.Page("why_play.py", title="Why play Paladins of the West Kingdom?", icon=":material/thumb_up:")
+#worker_placement = st.Page("worker_placement.py", title="What is a worker placement game?", icon = ":material/thumb_up:")
+#aim = st.Page("aim.py", title="What is the aim of the game?", icon=":material/thumb_up:")
 
-pg = st.navigation([why, worker_placement, aim])
-st.set_page_config(page_title="POTWK Tutorial", page_icon=":material/thumb_up:")
-pg.run()
+#pg = st.navigation([why, worker_placement, aim])
+#st.set_page_config(page_title="POTWK Tutorial", page_icon=":material/thumb_up:")
+#pg.run()
 
 
