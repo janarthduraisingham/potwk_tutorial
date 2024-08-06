e4f718db0b81f97d42832545abeafe18c722eb64
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-07 00:34:56 +0100

    added aim.py

---
+++ aim.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 00:32:30 2024
+
+@author: jd_se
+"""
+import streamlit as st
+
+st.title("What is the aim of the game?")
+
+st.subheader("Now, you may want to check out:")
+st.write("What is a worker placement game")
+st.write("Game setup")
--- potwk_tutorial.py
+++ potwk_tutorial.py
@@ -7,8 +7,9 @@ Created on Tue Aug  6 11:51:36 2024
 import streamlit as st
 
 worker_placement = st.Page("worker_placement.py", title="What is a worker placement game?", icon = ":material/thumb_up:")
+aim = st.Page("aim.py", title="What is the aim of the game?", icon=":material/thumb_up:")
 
-pg = st.navigation([worker_placement])
+pg = st.navigation([worker_placement, aim])
 st.set_page_config(page_title="POTWK Tutorial", page_icon=":material/thumb_up:")
 pg.run()
 
