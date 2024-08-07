0c8af696a0613e7414f70eca678306f78150be15
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-07 01:19:24 +0100

    tried video

--- why_play.py
+++ why_play.py
@@ -7,6 +7,11 @@ import streamlit as st
 
 st.header("Why should you play Paladins of the West Kingdom?")
 
+
+video_file = open("videos/test.mp4", "rb")
+test = video_file.read()
+st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+
 st.subheader("Now, you may want to check out:")
 st.write("What is a worker placement game?")
 st.write("What is the aim of the game?")
