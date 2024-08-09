9b3bdc4bc6dcca6e709a36974031c8a97d50b8c4
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-09 17:24:51 +0100

    experimenting

--- calculator.py
+++ calculator.py
@@ -11,22 +11,29 @@ st.header("34) Score Calculator")
 
 cols = st.columns(4)
 
-with cols[0]:
-    
-    p1 = st.text_input("Player 1 Name")
-    
-with cols[1]:
-    
-    p2 = st.text_input("Player 2 Name")
-    
-with cols[2]:
-    
-    p3 = st.text_input("Player 3 Name")
-    
-with cols[3]:
-    
-    p4 = st.text_input("Player 4 Name")
+var = {}
 
-st.write(p1+p2+p3+p4)
+for i in range(4):
+    
+    with cols[i]:
+        
+        var["name"+str(i)] = st.text_input("Player " + str(i+1) + " Name")
+#with cols[0]:
+    
+    #p1 = st.text_input("Player 1 Name")
+    
+#with cols[1]:
+    
+    #p2 = st.text_input("Player 2 Name")
+    
+#with cols[2]:
+    
+    #p3 = st.text_input("Player 3 Name")
+    
+#with cols[3]:
+    
+    #p4 = st.text_input("Player 4 Name")
+
+st.write(var["name1"])
 
 
