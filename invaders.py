d4c3cc4ee6e433fe8988f5ef449a303e41e60a38
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-09 16:00:20 +0100

    invaders links

--- invaders.py
+++ invaders.py
@@ -16,11 +16,6 @@ st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, 
 st.subheader("Next up:")
 if st.button("7) Silver and Bronze"):
     st.switch_page("silver_bronze.py")
-
-st.write("Questions? Don't worry - later, we'll look at")    
-
-if st.button("14) Actions"):
-    st.switch_page("actions.py")
     
 st.write("Went on a tangent? Return to:")
 if st.button("3) What is the aim of the game?"):
